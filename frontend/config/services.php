db450a7a85ba39bd16dd86d5e16112663aee6cbc
Nick <pyatakn@gmail.com>
2019-03-07 14:22:20 +0300

    upd

--- frontend/config/services.php
+++ frontend/config/services.php
@@ -3,8 +3,8 @@ return [
     /*'fb' => [
         // register your app here: https://developers.facebook.com/apps/
         'class' => 'frontend\models\social\FbOAuth2Service',
-        'clientId' => '506014906472268',
-        'clientSecret' => '3bcf4d406d396a10ec82332fc26a596e',
+        'clientId' => '502516763613354',
+        'clientSecret' => 'b4cbc20cef191563f0c1e1c5d0450cad',
         'title' => 'Facebook',
     ],*/
     'vk' => [
@@ -18,9 +18,9 @@ return [
         // register your app here: http://dev.odnoklassniki.ru/wiki/pages/viewpage.action?pageId=13992188
         // ... or here: http://www.odnoklassniki.ru/dk?st.cmd=appsInfoMyDevList&st._aid=Apps_Info_MyDev
         'class' => 'frontend\models\social\OkOAuth2Service',
-        'clientId' => '1275412992',
-        'clientPublic' => 'CBADEGANEBABABABA',
-        'clientSecret' => '7B4384B41E808C5C50C8FDDF',
+        'clientId' => '1276841472',
+        'clientPublic' => 'CBAPAMBNEBABABABA',
+        'clientSecret' => 'ADD8797ED0DF9E0D9C0279E0',
         'title' => 'Одноклассники',
     ],
 ];
